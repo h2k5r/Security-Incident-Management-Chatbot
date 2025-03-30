77ce2c9b8c6e2646f751aeb6ffacd47472519aa7
h2k5r <52317683+h2k5r@users.noreply.github.com>
2025-03-30 18:45:37 +0530

    reset

--- run.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
